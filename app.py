709ccf6f6f032f7572480e9f7d78bcc7a549c07e
Shari87 <sahajhaksh@gmail.com>
2020-11-01 23:21:41 +0530

    Added comments and modified the versioning function

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
+
+"""
+A simple restful application to display its current version
+"""
+__author__ = "Sahajhaksh Hariharan"
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/version/<float:num>', methods=['GET'])
-def display_version(num):
+@app.route('/<float:version>', methods=['GET']) 
+def disp_version(version):
     """
     :param num: a float argument to display the current version of the app
     :return: a json output which displays the current version
     """
-    return jsonify({'The version of the app is': num})
-
+    return jsonify({'The version of the app is': version})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
