754be808e5bccab769b961b50a38e293526c89eb
Shari87 <sahajhaksh@gmail.com>
2020-11-01 21:43:44 +0530

    Modified route 53 record and acm cert validation to newer version

--- terraform/ecs_fargate/2-platform/domain.tf
+++ terraform/ecs_fargate/2-platform/domain.tf
@@ -12,16 +12,38 @@ data "aws_route53_zone" "ecs_domain" {
   private_zone = false
 }
 
+# resource "aws_route53_record" "ecs_cert_validation_record" {
+#   name            = "tolist(aws_acm_certificate.ecs_domain_certificate.domain_validation_options)[0].resource_record_name"
+#   type            = "tolist(aws_acm_certificate.ecs_domain_certificate.domain_validation_options)[0].resource_record_type}"
+#   zone_id         = data.aws_route53_zone.ecs_domain.zone_id
+#   records         = [tolist(aws_acm_certificate.ecs_domain_certificate.domain_validation_options)[0].resource_record_value]
+#   ttl             = 60
+#   allow_overwrite = true
+# }
+
+# resource "aws_acm_certificate_validation" "ecs_domain_certificate_validation" {
+#   certificate_arn         = aws_acm_certificate.ecs_domain_certificate.arn
+#   validation_record_fqdns = [aws_route53_record.ecs_cert_validation_record.fqdn]
+# }
+
 resource "aws_route53_record" "ecs_cert_validation_record" {
-  name            = aws_acm_certificate.ecs_domain_certificate.domain_validation_options[0].resource_record_name
-  type            = aws_acm_certificate.ecs_domain_certificate.domain_validation_options[0].resource_record_type
-  zone_id         = data.aws_route53_zone.ecs_domain.zone_id
-  records         = [aws_acm_certificate.ecs_domain_certificate.domain_validation_options[0].resource_record_value]
-  ttl             = 60
+  for_each = {
+    for dvo in aws_acm_certificate.ecs_domain_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.ecs_domain.zone_id
 }
 
 resource "aws_acm_certificate_validation" "ecs_domain_certificate_validation" {
   certificate_arn         = aws_acm_certificate.ecs_domain_certificate.arn
-  validation_record_fqdns = [aws_route53_record.ecs_cert_validation_record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.ecs_cert_validation_record : record.fqdn]
 }
