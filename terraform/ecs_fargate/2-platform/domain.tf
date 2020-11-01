e9fc7200386f7d777e34672ce12d16058aacf6e0
Shari87 <sahajhaksh@gmail.com>
2020-11-02 01:48:03 +0530

    Removed commented source code

--- terraform/ecs_fargate/2-platform/domain.tf
+++ terraform/ecs_fargate/2-platform/domain.tf
@@ -12,20 +12,6 @@ data "aws_route53_zone" "ecs_domain" {
   private_zone = false
 }
 
-# resource "aws_route53_record" "ecs_cert_validation_record" {
-#   name            = "tolist(aws_acm_certificate.ecs_domain_certificate.domain_validation_options)[0].resource_record_name"
-#   type            = "tolist(aws_acm_certificate.ecs_domain_certificate.domain_validation_options)[0].resource_record_type}"
-#   zone_id         = data.aws_route53_zone.ecs_domain.zone_id
-#   records         = [tolist(aws_acm_certificate.ecs_domain_certificate.domain_validation_options)[0].resource_record_value]
-#   ttl             = 60
-#   allow_overwrite = true
-# }
-
-# resource "aws_acm_certificate_validation" "ecs_domain_certificate_validation" {
-#   certificate_arn         = aws_acm_certificate.ecs_domain_certificate.arn
-#   validation_record_fqdns = [aws_route53_record.ecs_cert_validation_record.fqdn]
-# }
-
 resource "aws_route53_record" "ecs_cert_validation_record" {
   for_each = {
     for dvo in aws_acm_certificate.ecs_domain_certificate.domain_validation_options : dvo.domain_name => {
