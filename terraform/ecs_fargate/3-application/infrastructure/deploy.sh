5b796762f44ac41305638acc423b62d89cfc22b4
Shari87 <sahajhaksh@gmail.com>
2020-11-03 21:40:18 +0530

    Added extra comments

--- terraform/ecs_fargate/3-application/infrastructure/deploy.sh
+++ terraform/ecs_fargate/3-application/infrastructure/deploy.sh
@@ -2,15 +2,16 @@
 
 SERVICE_NAME="app"
 SERVICE_TAG="v1"
-ECR_REPO_URL="AMAZON_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/${SERVICE_NAME}" # Please add your respective aws account id in place of AMAZON_ACCOUNT_ID
+ECR_REPO_URL="AMAZON_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/${SERVICE_NAME}" # Please add your respective aws account id in place of AMAZON_ACCOUNT_ID before running this script
 
 
 if [ "$1" = "dockerize" ];then
-    sudo docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) AMAZON_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com # Please add your respective aws account id in place of AMAZON_ACCOUNT_ID
+    sudo docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) AMAZON_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com # Please add your respective aws account id in place of AMAZON_ACCOUNT_ID before running this script
     sudo aws ecr create-repository --repository-name ${SERVICE_NAME:?} || true
     sudo docker image build -t ${SERVICE_NAME}:${SERVICE_TAG} .
     sudo docker tag ${SERVICE_NAME}:${SERVICE_TAG} ${ECR_REPO_URL}:${SERVICE_TAG}
-    sudo docker push ${ECR_REPO_URL}:${SERVICE_TAG}
+    
+sudo docker push ${ECR_REPO_URL}:${SERVICE_TAG}
 elif [ "$1" = "plan" ];then
     terraform init -backend-config="app-prod.config"
     terraform plan -var-file="production.tfvars" -var "docker_image_url=${ECR_REPO_URL}:${SERVICE_TAG}"
